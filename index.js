851a3479af3c9c571c6036b2a9c6597de08b9e9b
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2023-05-16 14:04:15 +0100

    removed unecessary code I wasn't using anymore

--- index.js
+++ index.js
@@ -3,30 +3,12 @@ const colourContainer = document.querySelector("#colour-container")
 
 
 
-
-//  const colourData = {
-//    hex: colourValue,
-//    mode: colourSelection,
-//  };
-
-//  const options = {
-//    method: "POST",
-//    body: JSON.stringify(colourData),
-//    headers: {
-//      "Content-Type": "application/json",
-//    },
-//  };
-
-
    form.addEventListener("submit", (e) => {
      e.preventDefault();
      const colourPicker = document.querySelector("#colour-picker");
      const colourId = document.querySelector("#colour-id");
      let colourValue = colourPicker.value;
      let colourSelection = colourId.value;
-
-     console.log(colourValue, colourSelection)
-     
     
 
      fetch(`https://www.thecolorapi.com/scheme?hex=${colourValue.slice(1)}&mode=${colourSelection}`)
